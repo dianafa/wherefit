82b0525d49d64fddbf8db0cb653aac0e0949cd67
dianafa <diana.falkowska@gmail.com>
2017-01-14 18:40:16 +0100

    add new activity and integrate with welcome screen

--- app/src/main/java/com/diana/wherefit/MainActivity.java
+++ app/src/main/java/com/diana/wherefit/MainActivity.java
@@ -1,10 +1,12 @@
 package com.diana.wherefit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showAll(View view) {
-        // show all activities from current neighbourhood
+        Intent intent = new Intent(this, ListActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.button_all);
+        //String message = editText.getText().toString();
+        String message = "dianka";
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
