ce3c2af4b32e052e1118abc0d4c653b9219c96ee
dianafa <diana.falkowska@gmail.com>
2017-01-21 19:39:10 +0100

    add listitemtype2

--- app/src/main/java/com/diana/wherefit/ClassListActivity.java
+++ app/src/main/java/com/diana/wherefit/ClassListActivity.java
@@ -1,17 +1,76 @@
 package com.diana.wherefit;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ClassListActivity extends AppCompatActivity {
+    ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_list);
+
+        listView = (ListView) findViewById(R.id.list);
+
+        List<String> objList = Arrays.asList(
+                "Zumba",
+                "ABT",
+                "Body Shape",
+                "Tabata",
+                "Trampoliny",
+                "Pump",
+                "TBC",
+                "Joga",
+                "Stretching"
+        );
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, android.R.id.text1, objList) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                text1.setText("a");
+                text2.setText("b");
+                return view;
+            }
+        };
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String itemValue = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
