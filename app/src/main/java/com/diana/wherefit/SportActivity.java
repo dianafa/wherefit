547ad0038d1ccdb85550de01ab147653c2fde438
Piotr Wilczyński <wilczynskipio@gmail.com>
2017-01-22 20:46:15 +0100

    fixed format creation

--- app/src/main/java/com/diana/wherefit/SportActivity.java
+++ app/src/main/java/com/diana/wherefit/SportActivity.java
@@ -1,11 +1,15 @@
 package com.diana.wherefit;
 
+import android.support.annotation.NonNull;
+
 import java.io.Serializable;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class SportActivity implements Comparable<SportActivity>, Serializable {
 
-    private Calendar calendar = Calendar.getInstance();
+    private static final String FORMAT = "hh:mm";
 
     private String name;
 
@@ -35,13 +39,12 @@ public class SportActivity implements Comparable<SportActivity>, Serializable {
         return getHour(startTime) + " - " + getHour(endTime) + " " + getName();
     }
 
-    private String getHour(long time) {
-        calendar.setTimeInMillis(time);
-        return calendar.get(Calendar.HOUR) + ":" + calendar.get(Calendar.MINUTE);
+    @Override
+    public int compareTo(@NonNull SportActivity o) {
+        return Long.compare(this.getStartTime(), o.getStartTime());
     }
 
-    @Override
-    public int compareTo(SportActivity o) {
-        return Long.compare(this.getStartTime(), o.getStartTime());
+    private String getHour(long time) {
+        return new SimpleDateFormat(FORMAT, Locale.getDefault()).format(new Date(time));
     }
 }
