10ff86e8d7d267e9105e68893459c5ef635a9b75
dianafa <diana.falkowska@gmail.com>
2017-02-01 23:21:46 +0100

    add Timeframe class and TimeframeTest

---
+++ app/src/main/java/com/diana/wherefit/pojo/Timeframe.java
@@ -0,0 +1,57 @@
+package com.diana.wherefit.pojo;
+
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Timeframe {
+    private long startDate;
+    private long endDate;
+
+    public Timeframe(long todayDate, String times, int weekday) {
+        String pattern = "^(\\d{1,2})[:.]?(\\d{2})?[\\s\\-]*(\\d{1,2})[:.]?(\\d{2})?";
+        String startHourString = "0",
+                endHourString = "0",
+                startMinuteString = "0",
+                endMinuteString = "0";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(times);
+
+        if (m.find()) {
+            startHourString = m.group(1);
+            startMinuteString = m.group(2);
+            endHourString = m.group(3);
+            endMinuteString = m.group(4);
+        } else {
+            System.out.println("nie bangla czas taki: " + times);
+        }
+
+        Calendar today = Calendar.getInstance();
+        today.setTimeInMillis(todayDate);
+
+        // find the difference between today and the closest date with given day of week
+        int daysGap = Math.abs(weekday - today.get(Calendar.DAY_OF_WEEK));
+
+        Calendar start = Calendar.getInstance();
+        start.setTimeInMillis(todayDate);
+
+        Calendar end = Calendar.getInstance();
+        end.setTimeInMillis(todayDate);
+
+        start.add(Calendar.DAY_OF_WEEK, daysGap);
+        end.add(Calendar.DAY_OF_WEEK, daysGap);
+        start.set(Calendar.HOUR, Integer.parseInt(startHourString));
+        end.set(Calendar.HOUR, Integer.parseInt(endHourString));
+
+        startDate = start.getTimeInMillis();
+        endDate = end.getTimeInMillis();
+    }
+
+    public long getEndDate() {
+        return endDate;
+    }
+
+    public long getStartDate() {
+        return startDate;
+    }
+}
---
+++ app/src/test/java/com/diana/wherefit/TimeframeTest.java
@@ -0,0 +1,58 @@
+package com.diana.wherefit;
+
+import com.diana.wherefit.pojo.Timeframe;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.assertEquals;
+
+public class TimeframeTest {
+    @Test
+    public void constructorTest() throws Exception {
+
+        String[] testCases = {
+            "8-9",
+            "8 - 9",
+            "8:00-9",
+            "8-9:00",
+            "08-09",
+            "08-09:00",
+            "08:00-09:00",
+            "8.00-9",
+            "8.00-9.00",
+            "8.00-9:00"
+        };
+
+        Calendar now = new GregorianCalendar(2017, 0, 30); // 30 Jan 2017 00:00
+        Calendar expectedStart = new GregorianCalendar(2017, 1, 1, 8, 0); //1st Feb 2017 8:00
+        Calendar expectedEnd = new GregorianCalendar(2017, 1, 1, 9, 0); //1st Feb 2017 9:00
+
+        for (String testCase : testCases) {
+            System.out.println("Test case: " + testCase);
+            Timeframe time = new Timeframe(now.getTimeInMillis(), testCase, Calendar.WEDNESDAY);
+
+            Calendar actualStart = Calendar.getInstance();
+            actualStart.setTimeInMillis(time.getStartDate());
+
+            Calendar actualEnd = Calendar.getInstance();
+            actualEnd.setTimeInMillis(time.getEndDate());
+
+            assertEquals(expectedStart.get(Calendar.MONTH), actualStart.get(Calendar.MONTH));
+            assertEquals(expectedStart.get(Calendar.DAY_OF_MONTH), actualStart.get(Calendar.DAY_OF_MONTH));
+            assertEquals(expectedStart.get(Calendar.HOUR), actualStart.get(Calendar.HOUR));
+            assertEquals(expectedStart.get(Calendar.MINUTE), actualStart.get(Calendar.MINUTE));
+
+            assertEquals(expectedEnd.get(Calendar.MONTH), actualEnd.get(Calendar.MONTH));
+            assertEquals(expectedEnd.get(Calendar.DAY_OF_MONTH), actualEnd.get(Calendar.DAY_OF_MONTH));
+            assertEquals(expectedEnd.get(Calendar.HOUR), actualEnd.get(Calendar.HOUR));
+            assertEquals(expectedEnd.get(Calendar.MINUTE), actualEnd.get(Calendar.MINUTE));
+
+            assertEquals(expectedStart.getTimeInMillis(),time.getStartDate());
+            assertEquals(expectedEnd.getTimeInMillis(), time.getEndDate());
+        }
+    }
+
+}
